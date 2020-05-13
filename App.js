c372e9d62439047a6f0ee8ed287fe3f2332a0c3c
Louis <louis@Louiss-Mac-mini.local>
2020-05-13 12:54:33 -0400

    Added Logo and Icons components

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { ScrollView, SafeAreaView } from 'react-native';
 import styled from 'styled-components';
 import Card from './components/Card';
+// import { Icon } from 'expo';
+import { Ionicons } from '@expo/vector-icons';
+import { NotificationIcon } from './components/Icons';
+import Logo from './components/Logo';
 
 const Container = styled.View`
   flex: 1;
@@ -42,26 +46,61 @@ const Subtitle = styled.Text`
   font-weight: 600;
   font-size: 15px;
   margin-left: 20px;
-  margin-top: 50px;
+  margin-top: 20px;
   text-transform: uppercase;
 `;
 
 export default function App() {
   return (
     <Container>
-      <TitleBar>
-        <Avatar source={require('./assets/avatar.jpg')} />
-        <Title>Welcome back, </Title>
-        <Name>Louis</Name>
-      </TitleBar>
-      <Subtitle>Continue Learning</Subtitle>
-      <Card
-        title="Styled Components"
-        image={require('./assets/background2.jpg')}
-        caption="React Native"
-        logo={require('./assets/logo-react.png')}
-        subtitle="5 of 12 sections"
-      />
+      <SafeAreaView>
+        <ScrollView>
+          <TitleBar>
+            <Avatar source={require('./assets/avatar.jpg')} />
+            <Title>Welcome back, </Title>
+            <Name>Louis</Name>
+            <NotificationIcon
+              style={{ position: 'absolute', right: 20, top: 5 }}
+            />
+          </TitleBar>
+          <ScrollView
+            style={{
+              flexDirection: 'row',
+              padding: 20,
+              paddingLeft: 12,
+              paddingTop: 30,
+            }}
+            horizontal={true}
+          >
+            <Logo
+              image={require('./assets/logo-framerx.png')}
+              text="Framer X"
+            />
+            <Logo image={require('./assets/logo-figma.png')} text="Figma" />
+          </ScrollView>
+          <Subtitle>Continue Learningaa</Subtitle>
+          <ScrollView
+            horizontal={true}
+            style={{ paddingBottom: 30 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <Card
+              title="Styled Components"
+              image={require('./assets/background2.jpg')}
+              caption="React Native"
+              logo={require('./assets/logo-react.png')}
+              subtitle="5 of 12 sections"
+            />
+            <Card
+              title="Styled Components 2"
+              image={require('./assets/background1.jpg')}
+              caption="React Native"
+              logo={require('./assets/logo-react.png')}
+              subtitle="5 of 12 sections"
+            />
+          </ScrollView>
+        </ScrollView>
+      </SafeAreaView>
     </Container>
   );
 }
