85cfea0bcd414d5449de594b424b4417b6f9ad2b
Louis <louis@Louiss-Mac-mini.local>
2020-05-13 11:32:38 -0400

    Added base UI and first card component

--- App.js
+++ App.js
@@ -1,19 +1,67 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text } from 'react-native';
+import styled from 'styled-components';
+import Card from './components/Card';
+
+const Container = styled.View`
+  flex: 1;
+  background-color: #f0f3f5;
+`;
+
+const Title = styled.Text`
+  font-size: 16px;
+  color: #b8bece;
+  font-weight: 500;
+`;
+
+const Name = styled.Text`
+  font-size: 20px;
+  color: #3c4560;
+  font-weight: bold;
+`;
+
+const TitleBar = styled.View`
+  width: 100%;
+  margin-top: 50px;
+  padding-left: 80px;
+`;
+
+const Avatar = styled.Image`
+  width: 44px;
+  height: 44px;
+  background: black;
+  border-radius: 22px;
+  margin-left: 20px;
+  position: absolute;
+  top: 0;
+  left: 0;
+`;
+
+const Subtitle = styled.Text`
+  color: #b8bece;
+  font-weight: 600;
+  font-size: 15px;
+  margin-left: 20px;
+  margin-top: 50px;
+  text-transform: uppercase;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Opensss up App.js to start working on your app!</Text>
-    </View>
+    <Container>
+      <TitleBar>
+        <Avatar source={require('./assets/avatar.jpg')} />
+        <Title>Welcome back, </Title>
+        <Name>Louis</Name>
+      </TitleBar>
+      <Subtitle>Continue Learning</Subtitle>
+      <Card
+        title="Styled Components"
+        image={require('./assets/background2.jpg')}
+        caption="React Native"
+        logo={require('./assets/logo-react.png')}
+        subtitle="5 of 12 sections"
+      />
+    </Container>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
